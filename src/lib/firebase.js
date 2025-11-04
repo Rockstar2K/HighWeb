63ae660f57160f117cfc49ea861933919a70507f
rocofernandezpagella@gmail.com <46428180+Rockstar2K@users.noreply.github.com>
2025-11-04 13:27:12 -0300

    firebase working

---
+++ src/lib/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCrwTfk5BfqK9ZJAN8VRB0GW37S24ii-OM",
+  authDomain: "high-design-agency.firebaseapp.com",
+  projectId: "high-design-agency",
+  storageBucket: "high-design-agency.firebasestorage.app",
+  messagingSenderId: "314940684837",
+  appId: "1:314940684837:web:ce77a59ea30d4c70295dcc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
